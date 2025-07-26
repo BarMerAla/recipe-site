29b6913653e7e0a4b7508e9b9f85f8f8582d6aea
BarMerAla <meeke.mr@bk.ru>
2025-07-26 14:09:41 +0500

    update allowed_hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["77.222.42.5", "77.222.42.5:8000", "localhost", "*"]
 
 
 # Application definition
